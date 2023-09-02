0c3389c7113b2c4efe14af552a029a5e9f17cfdc
alvinwu01 <aw3254@columbia.edu>
2023-09-01 20:24:52 -0400

    Method to check visibile tiles

--- runelite-mixins/src/main/java/net/unethicalite/mixins/HClientMixin.java
+++ runelite-mixins/src/main/java/net/unethicalite/mixins/HClientMixin.java
@@ -69,6 +69,12 @@ public abstract class HClientMixin implements RSClient
 		}
 	}
 
+	@Inject
+	public boolean[][] getVisibleTiles()
+	{
+		return client.getVisibilityMaps()[(client.getCameraPitch() - 128) / 32][client.getCameraYaw() / 64];
+	}
+
 	@Copy("drawModelComponents")
 	@Replace("drawModelComponents")
 	static void copy$drawModelComponents(Widget[] var0, int var1)
