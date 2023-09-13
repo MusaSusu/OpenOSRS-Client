59b2ae13569c017a1da24d0be4778383b263519e
alvinwu01 <aw3254@columbia.edu>
2023-09-13 18:44:38 -0400

    removed unnecessary code

--- runelite-client/src/main/java/net/unethicalite/api/movement/Antiban.java
+++ runelite-client/src/main/java/net/unethicalite/api/movement/Antiban.java
@@ -1,16 +1,8 @@
 package net.unethicalite.api.movement;
 
-import net.runelite.api.Client;
-import net.unethicalite.api.input.naturalmouse.NaturalMouse;
 import net.unethicalite.client.Static;
 
-import javax.inject.Inject;
-
 public class Antiban {
-    @Inject
-    private Client client;
-    @Inject
-    private static NaturalMouse naturalMouse;
 
     public static void randomMouseMove()
     {
